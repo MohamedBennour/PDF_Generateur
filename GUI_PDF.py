c63f3918060eddf1afc4dbedb030a2f191759cd5
MohamedBennour <mohamed8ennour@gmail.com>
2024-04-28 17:43:10 +0100

    Version 2.0

--- GUI_PDF.py
+++ GUI_PDF.py
@@ -1,8 +1,9 @@
 from main import *
 import sys
-import pyodbc
+import pandas as pd
+import os
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QPushButton,
-                             QWidget, QDialog, QLabel, QLineEdit, QDialogButtonBox, QHBoxLayout, QMessageBox)
+                             QWidget, QDialog, QLabel, QLineEdit, QDialogButtonBox, QHBoxLayout, QMessageBox, QHeaderView)
 
 class App(QMainWindow):
     def __init__(self):
@@ -14,32 +15,40 @@ class App(QMainWindow):
         self.height = 600
         self.current_page = 1
         self.records_per_page = 10
+        self.dataFrame = pd.DataFrame() 
+        self.load_data_from_excel()  
         self.initUI()
+        self.load_data_to_table()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-         # Layouts
+        # Layouts
         main_layout = QVBoxLayout()
         search_layout = QHBoxLayout()
 
         # Créer les zones de recherche
-        self.search_numope = QLineEdit(self)
-        self.search_numope.setPlaceholderText("Rechercher par numéro d'opération")
+        self.search_dossier = QLineEdit(self)
+        self.search_dossier.setPlaceholderText("Rechercher par numéro de dossier")
         self.search_datval = QLineEdit(self)
         self.search_datval.setPlaceholderText("Rechercher par date de validation")
         self.search_datech = QLineEdit(self)
         self.search_datech.setPlaceholderText("Rechercher par date de paiement")
         self.search_devach = QLineEdit(self)
         self.search_devach.setPlaceholderText("Rechercher par devise")
+
+        self.search_dossier.returnPressed.connect(self.on_search_click)
+        self.search_datval.returnPressed.connect(self.on_search_click)
+        self.search_datech.returnPressed.connect(self.on_search_click)
+        self.search_devach.returnPressed.connect(self.on_search_click)
         
         # Bouton pour exécuter la recherche
         self.search_button = QPushButton('Rechercher', self)
         self.search_button.clicked.connect(self.on_search_click)
 
         # Ajouter les éléments de recherche au layout de recherche
-        search_layout.addWidget(self.search_numope)
+        search_layout.addWidget(self.search_dossier)
         search_layout.addWidget(self.search_datval)
         search_layout.addWidget(self.search_datech)
         search_layout.addWidget(self.search_devach)
@@ -51,10 +60,15 @@ class App(QMainWindow):
         self.tableWidget = QTableWidget()
         self.tableWidget.setColumnCount(7)
         self.tableWidget.setHorizontalHeaderLabels([
-            "Numéro Opération", "Date Validation", "Montant Net", "Devise",
+            "Numéro Dossier", "Date Validation", "Montant Net", "Devise",
             "Taux de Marge", "Marge Revenant", "Date de Paiement"
         ])
         main_layout.addWidget(self.tableWidget)
+        self.tableWidget.setSortingEnabled(True)
+        self.tableWidget.setEditTriggers(QTableWidget.NoEditTriggers)
+        self.tableWidget.setSelectionBehavior(QTableWidget.SelectRows)
+        self.tableWidget.setSelectionMode(QTableWidget.SingleSelection)
+        self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         pagination_layout = QHBoxLayout()
         self.btn_previous = QPushButton('Précédent', self)
@@ -78,48 +92,77 @@ class App(QMainWindow):
         self.setCentralWidget(central_widget)
         self.show()
 
-        self.load_data_from_db()
-
-    def load_data_from_db(self):
-        offset = (self.current_page - 1) * self.records_per_page
-        query = "SELECT NUMOPE, DATVAL, MNETAC, DEVACH, TAUXACH, MARACH, DATECH FROM hope ORDER BY NUMOPE OFFSET ? ROWS FETCH NEXT ? ROWS ONLY"
-        try:
-            with pyodbc.connect('DRIVER={SQL Server}; SERVER=DESKTOP-KIO15OT;DATABASE=BH_BANK; Trusted_Connection=yes;') as conn:
-                with conn.cursor() as cursor:
-                    cursor.execute(query, (offset, self.records_per_page))
-                    records = cursor.fetchall()
-                    self.tableWidget.setRowCount(len(records))
-                    for row_index, row_data in enumerate(records):
-                        for column_index, data in enumerate(row_data):
-                            self.tableWidget.setItem(row_index, column_index, QTableWidgetItem(str(data)))
-            self.update_page_info()
-        except Exception as e:
-            QMessageBox.critical(self, "Database Error", f"Error connecting to database: {e}")
-
+    def max_pages(self):
+        # Retourne le nombre total de pages
+        total_records = len(self.filtered_dataFrame)
+        return (total_records + self.records_per_page - 1) // self.records_per_page
+    
     def update_page_info(self):
-        self.lbl_page_info.setText(f"Page {self.current_page}")
+        # Met à jour le label avec le numéro de la page actuelle et le nombre total de pages
+        total_pages = self.max_pages()
+        self.lbl_page_info.setText(f"Page {self.current_page} de {total_pages}")
+        self.btn_previous.setEnabled(self.current_page > 1)
+        self.btn_next.setEnabled(self.current_page < total_pages)
+
+    def load_data_from_excel(self):
+        directory = 'Rapport'
+        self.original_dataFrame = pd.DataFrame()  # Stocker les données originales
+        for filename in os.listdir(directory):
+            if filename.endswith('.xlsx'):
+                file_path = os.path.join(directory, filename)
+                df = pd.read_excel(file_path)
+                self.original_dataFrame = pd.concat([self.original_dataFrame, df], ignore_index=True)
+        self.filtered_dataFrame = self.original_dataFrame.copy()  # Utiliser pour filtrage et affichage
+
+
+    def load_data_to_table(self):
+        self.update_page_info()  # Assurez-vous de mettre à jour l'info de pagination
+        start = (self.current_page - 1) * self.records_per_page
+        end = start + self.records_per_page
+        current_data = self.filtered_dataFrame.iloc[start:end]
+
+        self.tableWidget.setRowCount(len(current_data))
+        column_names = ['DOSSIER', 'DATVAL', 'MNETAC', 'DEVACH', 'TAUXACH', 'MARACH', 'DATECH']
+        for row_index, row_data in enumerate(current_data.itertuples(index=False)):
+            for column_index, column_name in enumerate(column_names):
+                data = getattr(row_data, column_name)
+                self.tableWidget.setItem(row_index, column_index, QTableWidgetItem(str(data)))
+        self.update_page_info()  # Mettre à jour l'info de pagination après chargement des données
 
     def on_search_click(self):
-        # Reset to first page on a new search
-        self.current_page = 1
-        self.load_data_from_db()
+        # Réinitialiser les données filtrées
+        self.filtered_dataFrame = self.original_dataFrame.copy()
+        # Appliquer les filtres
+        filters = {
+            'DOSSIER': self.search_dossier.text(),
+            'DATVAL': self.search_datval.text(),
+            'DATECH': self.search_datech.text(),
+            'DEVACH': self.search_devach.text()
+        }
+        mask = pd.Series(True, index=self.filtered_dataFrame.index)
+        for col, value in filters.items():
+            if value:
+                mask &= self.filtered_dataFrame[col].astype(str).str.contains(value, case=False, na=False)
+        self.filtered_dataFrame = self.filtered_dataFrame[mask]
+        self.current_page = 1  # Réinitialiser la pagination
+        self.load_data_to_table()
+
 
     def on_previous_click(self):
         if self.current_page > 1:
             self.current_page -= 1
-            self.load_data_from_db()
+            self.load_data_to_table()
 
     def on_next_click(self):
-        # Here you might want to check if there are still records to fetch to decide whether to increment page
         self.current_page += 1
-        self.load_data_from_db()
+        self.load_data_to_table()
 
     def on_click(self):
         row = self.tableWidget.currentRow()
         if row == -1:
             QMessageBox.warning(self, "Selection Error", "Please select an operation to generate its PDF.")
             return
-        num_ope = self.tableWidget.item(row, 0).text()
+        num_dossier = self.tableWidget.item(row, 0).text()
 
         buyer_dialog = QDialog()
         buyer_dialog.setWindowTitle("Entrer Nom de l'Acheteur")
@@ -127,7 +170,7 @@ class App(QMainWindow):
         label = QLabel("Nom de l'acheteur:")
         self.buyer_name_input = QLineEdit()
         button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
-        button_box.accepted.connect(lambda: self.validate_buyer_name(buyer_dialog, num_ope))
+        button_box.accepted.connect(lambda: self.validate_buyer_name(buyer_dialog, num_dossier))
         button_box.rejected.connect(buyer_dialog.reject)
         layout.addWidget(label)
         layout.addWidget(self.buyer_name_input)
@@ -135,11 +178,10 @@ class App(QMainWindow):
         buyer_dialog.setLayout(layout)
         buyer_dialog.exec()
 
-    def validate_buyer_name(self, dialog, num_ope):
+    def validate_buyer_name(self, dialog, num_dossier):
         buyer_name = self.buyer_name_input.text().strip()
         if buyer_name:
-            print(f"Génération du PDF pour l'opération {num_ope} avec l'acheteur {buyer_name}")
-            generatePDF(num_ope, buyer_name)
+            generatePDF(num_dossier, buyer_name)
             dialog.accept()
         else:
             error_dialog = QMessageBox()
--- main.py
+++ main.py
@@ -1,98 +1,121 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from datetime import datetime
-import pyodbc
+import pandas as pd
+import os
 import webbrowser
 
-# Connexion à la base de données
-conn = pyodbc.connect('DRIVER={SQL Server}; SERVER=DESKTOP-KIO15OT;DATABASE=BH_BANK; Trusted_Connection=yes;')
-cursor = conn.cursor()
+# Chemin vers le répertoire contenant les fichiers Excel
+directory = 'Rapport'
 
+def generatePDF(num_dossier, acheteur):
+    dossier_found = False  # Indicateur pour vérifier si le dossier est trouvé
 
-def generatePDF(num_ope, acheteur):
-    # Importation des données 
-    cursor.execute("SELECT DATVAL, MNETAC, DEVACH, TAUXACH, MARACH, DATECH FROM hope WHERE NUMOPE = ?", num_ope)
-    datval, mnetac, devach, tauxach, marach, datech = cursor.fetchone()
+    # Parcourir tous les fichiers Excel dans le répertoire spécifié
+    for filename in os.listdir(directory):
+        if filename.endswith('.xlsx'):
+            # Chemin complet vers le fichier
+            file_path = os.path.join(directory, filename)
+            
+            # Lecture du fichier Excel
+            df = pd.read_excel(file_path)
+            
+            # Filtrer pour le numéro de dossier spécifique
+            filtered_df = df.loc[df['DOSSIER'].astype(str).str.strip() == str(num_dossier).strip(), ['DOSSIER', 'LIBLON', 'DATVAL', 'MNETAC', 'DEVACH', 'TAUXACH', 'MARACH', 'DATECH']]
+ 
+            
+            # Vérifier si des données ont été trouvées
+            if not filtered_df.empty:
+                dossier_found = True
+                data = filtered_df.iloc[0]
+                
+                # Extraire les données nécessaires
+                date_op = data['DATVAL'].strftime('%d/%m/%Y')
+                date_paie = data['DATECH'].strftime('%d/%m/%Y')
+                montant = f"{float(data['MNETAC']):,.2f} {data['DEVACH']}"
+                marge_revenant = f"{float(data['MARACH']):,.2f} {data['DEVACH']}"
+                total_principal_profit = f"{float(data['MNETAC']) + float(data['MARACH']):,.2f} {data['DEVACH']}"
 
-    # Formatage des dates
-    date_op = datetime.strptime(datval, '%Y-%m-%d').strftime('%d/%m/%Y')
-    date_paie = datetime.strptime(datech, '%Y-%m-%d').strftime('%d/%m/%Y')
+                # Créer un PDF
+                c = canvas.Canvas(f"rapportf{num_dossier}.pdf", pagesize=A4)
+                c.setFont("Helvetica", 11)
+                c.setFont("Helvetica-Bold", 11)
+                c.drawRightString(500, 770, f"Tunis, le {date_op}")
+                c.drawRightString(500, 750, data['LIBLON'])
+                c.drawCentredString(300, 700, "RAPPORT D’EXECUTION")
+                c.drawCentredString(300, 680, "D’OPPERATION D’INVESTISSEMENT")
+                c.setFont("Helvetica", 11)
 
-    # Calcul des montants
-    montant = f"{float(mnetac):,.2f} {devach}"
-    marge_revenant = f"{float(marach):,.2f} {devach}"
-    total_principal_profit = f"{float(mnetac) + float(marach):,.2f} {devach}"
+                c.drawString(50, 640, "En application de la convention cadre nous avons l’honneur de vous communiquer les détails de")
+                c.drawString(50, 620, "l’opération d’investissement réalisée le :")
 
-    # Créer un PDF
-    c = canvas.Canvas(f"rapportf{num_ope}.pdf", pagesize=A4)
-    c.setFont("Helvetica", 11)
+                # Détails de l'opération
+                y = 600 
+                line_height = 20
+                center_page = A4[0] / 2  # A4[0] représente la largeur de la page
 
-    # En-tête du rapport
-    c.setFont("Helvetica-Bold", 11)
-    c.drawRightString(500, 750, f"Tunis, le {date_op}")
-    c.drawCentredString(300, 700, "RAPPORT D’EXECUTION")
-    c.drawCentredString(300, 680, "D’OPPERATION D’INVESTISSEMENT")
-    c.setFont("Helvetica", 11)
-    c.drawString(50, 640, "En application de la convention cadre nous avons l’honneur de vous communiquer les détails de")
-    c.drawString(50, 620, "l’opération d’investissement réalisée le :")
+                # En-tête de la section détail
+                c.setFont("Helvetica-Bold", 11)
 
-    # Détails de l'opération
-    y = 600 
-    line_height = 20
-    center_page = A4[0] / 2  # A4[0] représente la largeur de la page
+                def drawDetailItem(title, detail, y):
+                    c.drawRightString(center_page - 20, y, title)
+                    c.drawString(center_page + 20, y, f": {detail}")
 
-    # En-tête de la section détail
-    c.setFont("Helvetica-Bold", 11)
+                drawDetailItem("Numéro de dossier", data['DOSSIER'], y)
+                y -= line_height
+                drawDetailItem("Date de l’opération", date_op, y)
+                y -= line_height
+                drawDetailItem("Montant", montant, y)
+                y -= line_height  
+                drawDetailItem("Acheteur", acheteur.upper(), y)
+                y -= line_height
+                drawDetailItem("Fournisseur", "DIVERS", y)
+                y -= line_height
+                objet_fin = "FINANCEMENT EN DINAR" if data['DEVACH'] == "TND" else "FINANCEMENT EN DEVISE"
+                drawDetailItem("Objet du financement", objet_fin, y)
+                y -= line_height
+                drawDetailItem("Taux de marge prévue", f"{data['TAUXACH']}%", y)
+                y -= line_height
+                drawDetailItem("Marge vous revenant", marge_revenant, y)
+                y -= line_height
+                drawDetailItem("Date de paiement", date_paie, y)
 
-    def drawDetailItem(title, detail, y):
-        c.drawRightString(center_page - 20, y, title)
-        c.drawString(center_page + 20, y, f": {detail}")
+                # Texte statique 
+                c.setFont("Helvetica", 11)
+                c.drawString(50, 420, "En outre, nous vous confirmons que :")
+                c.drawString(50, 400, "- Notre client (acheteur) indiqué ne présente aucun impayé dans le secteur et présente des")
+                c.drawString(50, 380, "perspectives futures rassurantes.")
+                c.drawString(50, 360, "- Tout incident de paiement futur constaté sur notre client (acheteur) vous sera signalé dans les")
+                c.drawString(50, 340, "plus brefs délais.")
+                c.drawString(50, 320, "Par ailleurs, nous vous créditeront chez BCT du montant en principal et profit réalisé,")
 
-    drawDetailItem("Date de l’opération", date_op, y)
-    y -= line_height
-    drawDetailItem("Montant", montant, y)
-    y -= line_height  
-    drawDetailItem("Acheteur", acheteur.upper(), y)
-    y -= line_height
-    drawDetailItem("Fournisseur", "DIVERS", y)
-    y -= line_height
-    objet_fin = "FINANCEMENT EN DINAR" if devach == "TND" else "FINANCEMENT EN DEVISE"
-    drawDetailItem("Objet du financement", objet_fin, y)
-    y -= line_height
-    drawDetailItem("Taux de marge prévue", f"{tauxach}%", y)
-    y -= line_height
-    drawDetailItem("Marge vous revenant", marge_revenant, y)
-    y -= line_height
-    drawDetailItem("Date de paiement", date_paie, y)
+                text_width = c.stringWidth("Soit ", "Helvetica", 11)
+                c.drawString(50, 300, "Soit ")
 
-    # Texte statique 
-    c.setFont("Helvetica", 11)
-    c.drawString(50, 420, "En outre, nous vous confirmons que :")
-    c.drawString(50, 400, "- Notre client (acheteur) indiqué ne présente aucun impayé dans le secteur et présente des")
-    c.drawString(50, 380, "perspectives futures rassurantes.")
-    c.drawString(50, 360, "- Tout incident de paiement futur constaté sur notre client (acheteur) vous sera signalé dans les")
-    c.drawString(50, 340, "plus brefs délais.")
-    c.drawString(50, 320, "Par ailleurs, nous vous créditeront chez BCT du montant en principal et profit réalisé,")
+                c.setFont("Helvetica-Bold", 11)
+                c.drawString(50 + text_width, 300, f"{total_principal_profit}")
+                text_width += c.stringWidth(f"{total_principal_profit}", "Helvetica-Bold", 11)
 
-    text_width = c.stringWidth("Soit ", "Helvetica", 11)
-    c.drawString(50, 300, "Soit ")
+                c.setFont("Helvetica", 11)
+                c.drawString(50 + text_width, 300, " valeur ")
+                text_width += c.stringWidth(" valeur ", "Helvetica", 11)
 
-    c.setFont("Helvetica-Bold", 11)
-    c.drawString(50 + text_width, 300, f"{total_principal_profit}")
-    text_width += c.stringWidth(f"{total_principal_profit}", "Helvetica-Bold", 11)
+                c.setFont("Helvetica-Bold", 11)
+                c.drawString(50 + text_width, 300, f"{date_paie}")
+                text_width += c.stringWidth(f"{date_paie}", "Helvetica-Bold", 11)
 
-    c.setFont("Helvetica", 11)
-    c.drawString(50 + text_width, 300, " valeur ")
-    text_width += c.stringWidth(" valeur ", "Helvetica", 11)
+                c.setFont("Helvetica", 11)
+                c.drawString(50 + text_width, 300, " et ce selon les termes et conditions prévus dans")
+                c.drawString(50, 280, "la convention cadre signée entre nos deux institutions.")
+                c.drawString(50, 260, "Salutations.")
+                c.setFont("Helvetica-Bold", 11)
+                c.drawString(50, 240, "BH Bank")
 
-    c.setFont("Helvetica-Bold", 11)
-    c.drawString(50 + text_width, 300, f"{date_paie}")
-    text_width += c.stringWidth(f"{date_paie}", "Helvetica-Bold", 11)
+                # Enregistrement et ouverture du PDF
+                c.save()
+                webbrowser.open_new(f"rapportf{num_dossier}.pdf")
+                break  # Sortir de la boucle après avoir généré le PDF
 
-    c.setFont("Helvetica", 11)
-    c.drawString(50 + text_width, 300, " et ce selon les termes et conditions prévus dans")
-    c.drawString(50, 280, "la convention cadre signée entre nos deux institutions.")
-    c.drawString(50, 260, "Salutations.")
+    if not dossier_found:
+        print("Aucun dossier correspondant trouvé dans les fichiers disponibles.")
 
-    c.save()
-    webbrowser.open_new(f"rapportf{num_ope}.pdf")
